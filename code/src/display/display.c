43ffd297cfc457979fceb1cd118611a396a63132
Stainvy <stainvy@gmail.com>
2025-09-26 01:45:37 +0200

    display

--- code/src/display/display.c
+++ code/src/display/display.c
@@ -2,8 +2,11 @@
 #include "display.h"
 
 void display (char** map) {
-    for (int )
+    for (char row = 'A'; row < 'A' + 10; row++) {
+        printf("\t%c", row);
+    }
     for(int row = 0; row < 10; row++) {
+        printf("%c\t", row);
         for(int column = 0; column < 10; column++) {
             printf("%c\t", map[row][column]);
         }
