15f746aed2174d0c6aac632cc2ade5ca05324f74
Nathan GERAULT <nathounngt@gmail.com>
2025-09-26 01:14:28 +0200

    Changes by narbo

--- code/src/display/display.c
+++ code/src/display/display.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "display.h"
 
-void dpl_print_table(const int i)
-{
-    printf("%d\n", i);
+void display (char** map) {
+    for (int )
+    for(int row = 0; row < 10; row++) {
+        for(int column = 0; column < 10; column++) {
+            printf("%c\t", map[row][column]);
+        }
+        printf("\n");
+    }
 }
